5ad1d4dfd5f4453b7b7ee40378fe3ad7ff4e28ad
KevinSig <41274608+KevinSig@users.noreply.github.com>
2022-12-12 09:13:12 -0500

    footer changes

--- src/partials/Cta.jsx
+++ src/partials/Cta.jsx
@@ -17,11 +17,11 @@ function Cta() {
 
             <div className="relative">
               <h2 className="h2 font-asap-display text-slate-800 mb-4">Instantly increase sales. Guaranteed.</h2>
-              <p className="text-xl text-slate-500 mb-8">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.</p>
+              <p className="text-xl text-slate-500 mb-8">Sell more to your existing customers and convert first-time visitors into repeat buyers.</p>
               <div>
-                <div className="btn text-white bg-blue-600 hover:bg-blue-700 group" >
-                  Request Demo <span className="tracking-normal text-blue-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
-                </div>
+                {/* <div className="btn text-white bg-blue-600 hover:bg-blue-700 group" >
+                  Sign Up <span className="tracking-normal text-blue-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                </div> */}
               </div>
             </div>
             
